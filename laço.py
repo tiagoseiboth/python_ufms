1e0dab0af7014598a595c1671219c309c8377cfc
Tiago Seiboth <seiboth1@gmail.com>
2021-04-26 14:01:23 -0300

    upload atividade laços python

---
+++ laço.py
@@ -0,0 +1,10 @@
+print("Pressione enter para calcular o valor de 5!")
+input()
+
+num_ini = 1
+
+for num in [1, 2, 3, 4, 5] :
+    num_ini = num_ini * num
+    print(num_ini, num)
+
+print("O valor do produto é =", num_ini)
