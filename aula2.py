069e3940d0dfc9f99af2ef016002093f5c8d5c76
Tiago Seiboth <seiboth1@gmail.com>
2021-04-27 20:22:23 -0300

    adicionado try e except

--- aula2.py
+++ aula2.py
@@ -1,25 +1,30 @@
 import os
 
 print("Quantas horas foram trabalhadas?")
-x = float(input())
-print("Qual o salario/h do funcionario em reais?")
-y = float(input())
-os.system('cls' if os.name == 'nt' else 'clear')
-
-if x > 40.00:
-    z = y*40
-    horas_extras = x-40 
-    s_horas = y*0.50+y
-    print("###############################################")
-    print("Horas extras trabalhadas:", horas_extras, "horas")
-    print("Salario/h para horas extras:", s_horas , "Reais")
-    print("Total pago em horas extras:", s_horas*horas_extras , "Reais")
-    print("Salario final:", z+(s_horas*horas_extras) , "Reais")
-    print("###############################################")
-
-else:
-    print("##############################################")
-    print("Salario total por horas trabalhas:", x*y , "Reais")
-    print("##############################################")
-
+x1 = input()
+try:
+    x = int(x1)
+    print("Qual o salario/h do funcionario em reais?")
+    y = float(input())
+    os.system('cls' if os.name == 'nt' else 'clear')
+    if x > 40.00:
+        z = y*40
+        horas_extras = x-40 
+        s_horas = y*0.50+y
+        h_extras = s_horas*horas_extras
+        s_final = z+(s_horas*horas_extras)
+        print("###############################################")
+        print("Horas extras trabalhadas:", horas_extras, "horas")
+        print("Salario/h para horas extras:", s_horas , "Reais")
+        print("Total pago em reais para as horas extras: %.2f" % h_extras)
+        print("Salario final em reais: %.2f" % s_final)
+        print("###############################################")
+    else:
+        s_total = x*y
+        print("##############################################")
+        print("Salario total por horas trabalhas: %.2f" % s_total)
+        print("##############################################")
+except:
+    x = -1
+    print("Valor incorreto para o numero de horas, por favor utilize somente numeros")
 os.system('pause' if os.name == 'nt' else 'sleep(5)')
