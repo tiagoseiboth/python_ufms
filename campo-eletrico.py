b8d3b35582868e103b396f0c567f84c01fa23b88
tiagoseiboth <82284229+tiagoseiboth@users.noreply.github.com>
2024-09-28 13:25:36 -0400

    Add files via upload

---
+++ campo-eletrico.py
@@ -0,0 +1,16 @@
+import numpy as np
+import math as m
+
+# constantes e parametros
+def f(x):
+    q1*(r2) - (q2*r1) == 0
+    return x
+
+k = 8.99e9
+q1 = float(input("Digite a carga q1 (em uC): "))
+q2 = float(input("Digite a carga q2 (em uC): "))
+r = float(input("Digite a distancia entre as cargas"))
+r2 = r**2 - 2*r*x + x**2
+r1 = x
+resultado = f(x)
+print(resultado)
